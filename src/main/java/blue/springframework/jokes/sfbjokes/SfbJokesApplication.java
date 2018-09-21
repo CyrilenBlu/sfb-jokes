9c571fb9c68979849e8d51565b647bb937dfc8d8
CyrilenBlu <lukepetzerint2@gmail.com>
2018-09-21 09:07:54 +0200

    Initial Project Add.

---
+++ src/main/java/blue/springframework/jokes/sfbjokes/SfbJokesApplication.java
@@ -0,0 +1,12 @@
+package blue.springframework.jokes.sfbjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfbJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfbJokesApplication.class, args);
+    }
+}
---
+++ src/test/java/blue/springframework/jokes/sfbjokes/SfbJokesApplicationTests.java
@@ -0,0 +1,16 @@
+package blue.springframework.jokes.sfbjokes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfbJokesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
